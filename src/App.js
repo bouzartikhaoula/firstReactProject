2b3c70f7b1856b0e61d7fab08a348dbf6a7668a0
khaoula <bouzartikhaoula@gmail.com>
2024-06-25 00:21:57 +0200

    part two

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React from 'react'
-import Home from './component/Home'
-import Recipes from './component/Recipes'
-import Blog from './component/Blog'
-import Contact from './component/Contact'
-import AboutUs from './component/AboutUs'
+import React from "react";
+import Home from "./component/Home";
+import Recipes from "./component/Recipes";
+import Blog from "./component/Blog";
+import Contact from "./component/Contact";
+import AboutUs from "./component/AboutUs";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
-      
       <BrowserRouter>
-<Routes>
-  <Route path='/' element={<Home/>}/>
-  <Route path='/recipes' element={<Recipes/>}/>
-  <Route path='/blog' element={<Blog/>}/>
-  <Route path='/contact' element={<Contact/>}/>
-  <Route path='/about' element={<AboutUs/>}/>
-
-</Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/component/Blog.js
+++ src/component/Blog.js
@@ -3,6 +3,12 @@ import Navigation from "./Navigation";
 import { Button } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import Image26 from "./image26.png";
+import Image27 from "./image27.png";
+import Image28 from "./image28.png";
+import Image29 from "./image29.png";
+
+
+
 const Blog = () => {
   return (
     <>
@@ -10,7 +16,7 @@ const Blog = () => {
       <Container>
         <div className="mx-auto  mt-5 text-center">
           <h1>Blog & Article</h1>
-          <p style={{ fontFamily: "initial", fontSize: "10px" }}>
+          <p  className="text-black-50" style={{ fontFamily: "initial", fontSize: "17px" }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore
           </p>
@@ -34,7 +40,7 @@ const Blog = () => {
         {/* // part 2 */}
         <div className="mx-auto row m-4 ">
           
-          <div className="col-8 row p-0 column-gap-3 ">
+          <div className="col-8 row p-0 column-gap-3 mb-3">
             
             {/* photo */}
             <div
@@ -50,19 +56,98 @@ const Blog = () => {
             {/* discription */}
             <div className="col">
               <h4>Crochet Projects for Noodle Lovers</h4>
-              <p>
+              <p className="text-black-50">
                 Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqut enim{" "}
               </p>
               <div className="m-0">
                 <spam>Wade Warren</spam>
-                <spam className="ms-4 ps-4 border-start">12 November 2021</spam>
+                <spam className="ms-4 ps-4 border-start text-black-50">12 November 2021</spam>
               </div>
             </div>
           </div>
-          {/* :::: */}
-          
+          {/* element2 */}
+          <div className="col-8 row p-0 column-gap-3 mb-3">
+            
+            {/* photo */}
+            <div
+              className="col-4 rounded-3"
+              style={{
+                border: "none",
+                backgroundImage: `url(${Image27})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+              }}
+            ></div>
+            {/* discription */}
+            <div className="col">
+              <h4>10 Vegetarian Recipes To Eat This Month</h4>
+              <p className="text-black-50">
+                Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqut enim{" "}
+              </p>
+              <div className="m-0">
+                <spam>Robert Fox</spam>
+                <spam className="ms-4 ps-4 border-start text-black-50">12 November 2021</spam>
+              </div>
+            </div>
+          </div>
+          {/* element3 */}
+          <div className="col-8 row p-0 column-gap-3 mb-3">
+            
+            {/* photo */}
+            <div
+              className="col-4 rounded-3"
+              style={{
+                border: "none",
+                backgroundImage: `url(${Image28})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+              }}
+            ></div>
+            {/* discription */}
+            <div className="col">
+              <h4>Full Guide to Becoming a Professional Chef</h4>
+              <p className="text-black-50">
+                Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqut enim{" "}
+              </p>
+              <div className="m-0">
+                <spam>Dianne Russell</spam>
+                <spam className="ms-4 ps-4 border-start text-black-50">12 November 2021</spam>
+              </div>
+            </div>
+          </div>
 
+          {/* element4 */}
+          <div className="col-8 row p-0 column-gap-3 mb-3">
+            
+            {/* photo */}
+            <div
+              className="col-4 rounded-3"
+              style={{
+                border: "none",
+                backgroundImage: `url(${Image29})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+              }}
+            ></div>
+            {/* discription */}
+            <div className="col">
+              <h4>Plantain and Pinto Stew with Aji Verde</h4>
+              <p className="text-black-50">
+                Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqut enim{" "}
+              </p>
+              <div className="m-0">
+                <spam>Courtney Henry</spam>
+                <spam className="ms-4 ps-4 border-start text-black-50">12 November 2021</spam>
+              </div>
+            </div>
+          </div>
           <div className="col-4">jj</div>
         </div>
       </Container>
